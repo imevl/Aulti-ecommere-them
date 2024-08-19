4c345819dd2b0f356e4e6524797e0e6c48766ad8
evl <163346138+imevl@users.noreply.github.com>
2024-08-19 14:54:28 +0100

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -49,7 +49,7 @@ const Contact = (props) => {
       <div className={styles.section}>
         <h4>Email</h4>
         <p>
-          You can email our Customer Service team at team@aulti.net
+          You can email our Customer Service team at team@aulti.store
           or via the contact form below:
         </p>
       </div>
