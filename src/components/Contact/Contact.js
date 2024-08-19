5e8aa4fd447a9068a6900a54250b793f676846c9
evl <163346138+imevl@users.noreply.github.com>
2024-08-19 13:05:04 +0100

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -39,7 +39,7 @@ const Contact = (props) => {
       <div className={styles.section}>
         <h4>Address</h4>
         <p>6545 Market Avenue North</p>
-        <p>North Canton,OH, 44720</p>
+        <p>North Canton, OH, 44720</p>
         <p>+1 424 280 4971</p>
         <p>Monday to Friday - 9am - 5pm AEDT</p>
       </div>
