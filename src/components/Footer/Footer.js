13f1825ffceffa6ef89d223934a4c08ca5f366ca
evl <163346138+imevl@users.noreply.github.com>
2024-08-19 13:31:02 +0100

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -69,10 +69,12 @@ const Footer = (prop) => {
             })}
             <div className={styles.newsLetter}>
               <div className={styles.newsLetterContent}>
-                <span className={styles.linkTitle}>Newsletter</span>
+                <span className={styles.linkTitle}>Contact Us!</span>
                 <p className={styles.promoMessage}>
-                  Get 15% off your first purchase! Plus, be the first to know
-                  about sales, new product launches and exclusive offers!
+                <p>6545 Market Avenue North</p>
+        <p>North Canton, OH, 44720</p>
+        <p>+1 424 280 4971</p>
+        <p>Monday to Friday - 9am - 5pm AEDT</p>
                 </p>
                 <form
                   className={styles.newsLetterForm}
